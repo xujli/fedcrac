32ae951d30833c8d1f8696b0b5c550858494de1b
xujing li <49027206+xujli@users.noreply.github.com>
2024-05-29 16:54:58 +0800

    Update net.py

--- models/net.py
+++ models/net.py
@@ -67,6 +67,94 @@ class modVGG(nn.Module):
         X = self.seq(X)
         return X
 
+class modVGG2(nn.Module):
+    def __init__(self, n_classes: int = 10, KD=False, projection=False, margin=0, mix=False):
+        super().__init__()
+        self.seq = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.3),
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.3),
+
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.3),
+
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.3),
+
+            nn.AdaptiveAvgPool2d(1),
+            nn.Flatten(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+        )
+        if mix:
+            Margin_method = SoftmaxMarginMix
+        else:
+            Margin_method = SoftmaxMargin
+        self.clf = nn.Linear(512, n_classes) if not margin else Margin_method(512, n_classes, margin=margin)
+        self.KD = KD
+        self.projection = projection
+        self.margin = margin != 0
+        self.relu = nn.ReLU()
+
+        if projection:
+            self.p1 = nn.Linear(512 * 1, 512 * 1)
+            self.p2 = nn.Linear(512 * 1, 256)
+            self.clf = nn.Linear(256, n_classes)
+
+    def forward(self, X: torch.Tensor, target=None):
+        x_f = self.seq(X)
+        if self.projection:
+            x_p = self.p1(x_f)
+            x_p = self.relu(x_p)
+            x_f = self.p2(x_p)
+        X = self.clf(x_f) if not self.margin else self.clf(x_f, target)
+
+        if self.KD == True:
+            return x_f, X
+        else:
+            return X
+
+    def vis(self, X):
+        X = self.seq(X)
+        return X
+
 
 class SimpleCNN(nn.Module):
     def __init__(self, n_classes: int = 10, KD=False, projection=False, margin=0):
